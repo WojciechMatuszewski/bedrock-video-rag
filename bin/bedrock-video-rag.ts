5b96c1ac648c364993a2dbdd607a26705671f877
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2024-08-12 05:58:52 +0200

    update

--- bin/bedrock-video-rag.ts
+++ bin/bedrock-video-rag.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { BedrockVideoRagStack } from "../lib/bedrock-video-rag-stack";
+import { BedrockVideoRagStack } from "../lib/bedrock-video-rag-stack.ts";
+import { IConstruct } from "constructs";
 
 const app = new cdk.App();
-new BedrockVideoRagStack(app, "BedrockVideoRagStack", {
+
+const stack = new BedrockVideoRagStack(app, "BedrockVideoRagStack", {
   synthesizer: new cdk.DefaultStackSynthesizer({
     qualifier: "bedrockrag"
   })
 });
+
+class RemovalPolicyAspect implements cdk.IAspect {
+  public visit(node: IConstruct): void {
+    if (node instanceof cdk.CfnResource) {
+      node.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+    }
+  }
+}
+
+cdk.Aspects.of(stack).add(new RemovalPolicyAspect());
--- lib/bedrock-video-rag-stack.ts
+++ lib/bedrock-video-rag-stack.ts
@@ -1,16 +1,66 @@
 import * as cdk from "aws-cdk-lib";
+import { IBucket } from "aws-cdk-lib/aws-s3";
+import { IStateMachine } from "aws-cdk-lib/aws-stepfunctions";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BedrockVideoRagStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const mediaBucket = new MediaBucket(this);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BedrockVideoRagQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const mediaStateMachine = new MediaStateMachine(this);
+
+    const mediaBus = new MediaBus(this, mediaBucket, mediaStateMachine);
+  }
+}
+
+class MediaBucket extends cdk.aws_s3.Bucket {
+  constructor(scope: Construct) {
+    super(scope, "MediaBucket", {
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      blockPublicAccess: cdk.aws_s3.BlockPublicAccess.BLOCK_ALL,
+      eventBridgeEnabled: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+  }
+}
+
+class MediaBus extends cdk.aws_events.EventBus {
+  constructor(scope: Construct, bucket: IBucket, stateMachine: IStateMachine) {
+    super(scope, "MediaBus");
+
+    const fileUploadedRule = new cdk.aws_events.Rule(this, "FileUploadedRule", {
+      enabled: true,
+      /**
+       * TODO: use the default bus here.
+       */
+      eventBus: this,
+      eventPattern: {
+        source: ["aws.s3"],
+        detailType: ["Object Created"],
+        detail: {
+          bucket: {
+            name: [bucket.bucketName]
+          }
+        }
+      }
+    });
+
+    fileUploadedRule.addTarget(
+      new cdk.aws_events_targets.SfnStateMachine(stateMachine)
+    );
+  }
+}
+
+class MediaStateMachine extends cdk.aws_stepfunctions.StateMachine {
+  constructor(scope: Construct) {
+    const passStep = new cdk.aws_stepfunctions.Pass(scope, "Pass");
+
+    super(scope, "MediaStateMachine", {
+      definitionBody:
+        cdk.aws_stepfunctions.DefinitionBody.fromChainable(passStep)
+    });
   }
 }
